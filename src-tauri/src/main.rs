098faca852ba4d64279253e2ed3b9044be99fdef
Pablushka <pablushka@nomades.com.ar>
2022-08-17 21:03:35 -0300

    First Commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,70 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+fn get_random_word() -> String {
+    let word: &str = "BANANA";
+    word.to_string()
+}
+
+/// Valida la cantidad de caracteres de word_guess
+fn is_word_guess_len_match(word_guess: &str, word: &str) -> bool {
+    let word_guess_len = word_guess.len();
+    let word_len = word.len();
+
+    println!(
+        "{} {} {}",
+        word_guess_len,
+        word_len,
+        word_guess.chars().last().unwrap()
+    );
+
+    if word_guess_len == word_len {
+        true
+    } else {
+        false
+    }
+}
+
+#[tauri::command]
+fn check_word(word_guess: String) -> String {
+    let word = get_random_word();
+
+    // while !is_word_guess_len_match(&word_guess, &word) {
+    //     println!("La palabra tiene {} caracteres", word.len());
+    //     word_guess = input_word();
+    // }
+
+    // B A N A N A
+    // R A B A N O
+    // x o ! o o x
+    // B A N A N A    'guess_chars
+
+    // Primer validacion: la cantidad de letras de la palabra es igual a la cantidad de letras del guess
+
+    // Segunda validacion: no puede haber simbolos, numeros o espacios en blanco
+
+    let mut guess_chars: Vec<char> = word.chars().collect();
+
+    word.chars().enumerate().for_each(|(i, c)| {
+        let test_char = word_guess.chars().nth(i).unwrap();
+        if c == test_char {
+            guess_chars[i] = 'o';
+        } else {
+            match word.find(test_char) {
+                Some(_) => guess_chars[i] = '!',
+                None => guess_chars[i] = 'x',
+            };
+        }
+    });
+
+    format!("{}", guess_chars.iter().collect::<String>())
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![check_word])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api'
+
+
+const handleSubmit = (e: any) => {
+  e.preventDefault();
+
+  // get value form input the word
+  const theWord = e.target.elements.the_word.value;
+
+  invoke('check_word', { wordGuess: theWord })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
+    .finally(() => {
+    }
+)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" onSubmit={handleSubmit}>
+        <input id="the_word" type="text" />
+        <button type='submit'>Enviar</button>
+      </form>
     </div>
   );
 }
