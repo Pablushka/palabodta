bf8970043e5dea6a346040b6c8aa801441a90474
Pablushka <pablushka@nomades.com.ar>
2022-09-07 21:08:14 -0300

    add some validatios, fix submit, fir word guessing

--- src/components/game-container/game-container.tsx
+++ src/components/game-container/game-container.tsx
@@ -34,54 +34,90 @@ const GameContainer = () => {
 	const [wordRows, setWordRows] = useState(InitialRows);
 	const [clues, setClues] = useState(InitialRows);
 	const [letterPos, setLetterPos] = useState(0);
+	const [turno, setTurno] = useState(0);
 
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	const handleSubmit = () => {
 
 		// agarrar la palabra correspondiente al turno
-		const theWord = wordRows[0].join('');
-
+		const theWord = wordRows[turno].join('');
 		invoke('check_word', { wordGuess: theWord })
 			.then((response) => {
 				let stringResponse: string = response as string;
-				let newClues: string[][] = [stringResponse.split('')]; // "xxoxxox" => ['x', 'x', 'o', 'x', 'x', 'o', 'x']
-				let oldClues: string[][] = clues.slice(1);
-				newClues = newClues.concat(oldClues);
+				let newClues = swapArray(turno, clues, [stringResponse.split('')])
 				setClues(newClues);
+				if (stringResponse.split('').every( (e)=> e === 'o') ) {
+					// eslint-disable-next-line no-restricted-globals
+					let rta = confirm('Gadaste!');
+					if (rta === true) {
+						setWordRows(InitialRows);
+						setClues(InitialRows);
+						setTurno(0);
+						setLetterPos(0);
+					};
+
+				}
+
 			})
 			.catch((error) => console.log(error))
 			.finally(() => {
 			})
 	}
 
-		const handleKeyDown = useCallback((event: KeyboardEvent) => {
+	// que hace esta funcion? (se ejecuta cada vez que se cambia el turno)
+	const swapArray = (pos: any, theArray: any, item: any): string[][] => {
+		let oldArray: string[][] = theArray.slice(0, pos);
+		let newArray = oldArray.concat(item, theArray.slice(pos + 1));
+
+		return newArray;
+	}
+
+	const handleKeyDown = useCallback((event: KeyboardEvent) => {
 		const { key, keyCode } = event;
 		// si es ENTER
 		if (keyCode === 13) {
 			// TODO: validar cantidad de letras antes del submit o hacer que check_word tire error??
-			handleSubmit();
+			console.log(wordRows[turno]);
+			if ((wordRows[turno].join('').length === 6) && (turno < 6)) {
+				handleSubmit();
+				setLetterPos(0);
+				setTurno(turno + 1);			
+			}
+	
 		}
-		if (keyCode >= 65 && keyCode <= 90) {
+
+		if (keyCode === 27) {
+			// TODO: validar cantidad de letras antes del submit o hacer que check_word tire error??
+
+			let newWord = swapArray(turno, wordRows, [['','','','','','']]);
+
+			// set state change
+			setWordRows(newWord);
+			setLetterPos(0);
+
+
+		}
+
+		if (keyCode >= 65 && keyCode <= 90 && letterPos <=5 && turno < 6) {
 			// TODO: tienen que completar la linea del turno actual
 		  // alert(`Key pressed ${key} \n Key code Value: ${keyCode}`);
 
 			// update wordRows with the new letter
 			// wordRows[0][1] = key;
 
-
-			let row = ['', '','','','',''];
-			row[letterPos] = key
-			console.log({letterPos});
-			const newWord: string[] = row;
+			let newWord: string[] = new Array().concat(wordRows[turno]);
+			newWord[letterPos] = key.toUpperCase();
 			let newLetterPos = letterPos + 1;
 			
 			let newWordRows: string[][] = [newWord]; // "xxoxxox" => ['x', 'x', 'o', 'x', 'x', 'o', 'x']
-			let oldWordRows: string[][] = wordRows.slice(1);
-			newWordRows = newWordRows.concat(oldWordRows);
-			setLetterPos(newLetterPos > 5 ? 0 : newLetterPos);
+			let oldWordRows: string[][] = wordRows.slice(0, turno);
+			newWordRows = oldWordRows.concat(newWordRows, wordRows.slice(turno + 1));
+
+			setLetterPos(newLetterPos);
 			setWordRows(newWordRows);
 			
 		}
-	}, []);
+	}, [handleSubmit, letterPos, wordRows, turno]);
 
 	useEffect(() => {
 		document.addEventListener('keydown', handleKeyDown, false);
