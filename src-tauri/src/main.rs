5862e72827c2df432584dbf4bba592d3e1a8a9c4
Pablushka <pablushka@nomades.com.ar>
2022-09-16 09:04:15 -0300

    creates words database

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,51 @@
     windows_subsystem = "windows"
 )]
 
+fn create_words_database() {
+    // :memory: is a special filename that tells SQLite to create an in-memory database
+    let connection = sqlite::open("./palabras.dat").unwrap();
+    connection
+        .execute("CREATE TABLE words (word VARCHAR(255))")
+        .unwrap();
+
+    // read a word from the palabras.json file
+    let words: Vec<String> = serde_json::from_str(include_str!("palabras.json")).unwrap();
+
+    println!("Start word insertion");
+    for word in words {
+        connection
+            .execute(format!("INSERT INTO words VALUES ('{}')", word).as_str())
+            .unwrap();
+    }
+    println!("Word insertion finished");
+}
+
 fn get_random_word() -> String {
     // https://palabras-aleatorias-public-api.herokuapp.com/random-by-length?length=6
+
+    // Forma 1
+    // 1. leer cada palabra del archivo y contar la cantidad de letras (ALTO)
+    // 2. crear un archivo nuevo que tenga como key la cantidad de letras de la pabras y la pabra
+
+    // archivo[] = ["banana",...]
+    // archivo[] = [ { "6": "banana" }, ... ] (ALTO)
+
+    // Forma 2
+    // 1. leer cada palabra del archivo e insertala en un bade de datos
+    // 2. hacer la busqueda de palabras por cantidad de letras en la base de datos
+
+    // BASE DE DATOS
+
+    // 1. La app nunca debe depender del motor de base de datos
+    // 2. Crear interfaces de conexion a la base de datos
+
+    // Interface de conexion a la base de datos
+    //
+    // Una funcion para obtener los datos: get_data()
+    // Una funcion para insertar los datos: insert_data()
+    // Una funcion para actualizar los datos: update_data()
+    // Una funcion para eliminar los datos: delete_data()
+
     let word: &str = "BANANA";
     word.to_string()
 }
